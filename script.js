df0780b1e28a4908f64affdee32a30d8e1fb66dd
Ezemine <32526879+Ezemine@users.noreply.github.com>
2021-05-05 17:13:46 -0500

    Final Lab
    Added a slider for interest rate, added scroll dropdown for years
    
    Changed background coloring for page and box fonts
    
    Fixed js to correctly compute

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year= new Date().getFullYear() + parseInt(years);
+
+    //Set result text
+    document.getElementById("result").innerHTML= "If you deposit " + principal + 
+    ",\<br\>at an interest rate of " + rate + 
+    "%\<br\>You will receive an amount of " +
+    interest + ",\<br\>in the year " + year + "\<br\>";
+
+    document.getElementById("interest").innerHTML = interest;
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+//validate principal
+function validateP()
+{
+    var principal = document.getElementById("principal").value;
+    if (parseInt(principal) <= 0 || isNaN(principal))
+    {
+        alert("Enter a number larger than 0.");
+        document.getElementById("principal").focus();
+    }
+}
